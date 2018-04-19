dedabbcd5de2e1c78ba461173cb77107d25df16f
Drew Meier <darius.montez@gmail.com>
2018-04-19 10:45:58 -0400

    Seperate requirements into separate file

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+with open('requirements.txt') as requirements_file:
+    requirements = [line for line in requirements_file]
 
 setup_requirements = ['pytest-runner', ]
 
